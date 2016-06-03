fcaded5c3a87e5dd71546fdf832c716d49e99f95
Dennis <dennis_wittmer@yahoo.fr>
2016-06-03 14:31:23 +0200

    Redirect after post form

--- app.js
+++ app.js
@@ -17,68 +17,83 @@ var db = new Db('employees', new Server('localhost', 27017));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 //Routes
 
-app.get('/accueil', function(req, res) {
-  res.render('index.ejs');
+app.get('/accueil', function (req, res) {
+    res.render('index.ejs');
 });
 
-app.get('/collaborateurs', function(req, res) {
-  db.open(function(err, db) {
-    var collection = db.collection("employee");
-    collection.find().toArray(function(err, result) {
-      if (err) return console.log(err)
-      res.render('liste-employes.ejs', {users: result});
+// Affiche la page de liste des employés
+app.get('/collaborateurs', function (req, res) {
+    // Cnx à la base
+    db.open(function (err, db) {
+        var collection = db.collection("employee");
+        // Requete sur la base + convertir en tableau les résultats
+        collection.find().toArray(function (err, result) {
+            if (err) return console.log(err)
+            // Retourne la vue avec comme param le résultat
+            res.render('liste-employes.ejs', {users: result});
+        });
     });
-  });
 });
 
-app.post('/ajouter_collaborateur', function(req, res) {
-    db.open(function(err, db) {
-    var collection = db.collection("employee");
-    var id = 6,
-        nom = req.body.nom,
-        prenom = req.body.prenom,
-        naissance = req.body.naissance,
-        entry = req.body.entry,
-        out = req.body.out, 
-        poste = req.body.poste,
-        salaire = req.body.salaire,
-        numero = req.body.numero,
-        mail = req.body.mail;
+// Affiche la vue d'ajout sur la requete GET
+app.get('/collaborateurs/new', function (req, res) {
+    res.render('form.ejs');
+});
 
-    var json = {
-      'id': id,
-      'name': nom,
-      'prenom': prenom,
-      'naissance': naissance,
-      'poste': poste,
-      'salaire': salaire,
-      'entry': entry,
-      'out': out,
-      'numero': numero,
-      'mail': mail
-    };
+// Requete d'ajout en base de données par requete POST
+app.post('/collaborateurs/new', function (req, res) {
+    // Cnx à la base
+    db.open(function (err, db) {
+        // Séléction de la collection
+        var collection = db.collection("employee");
 
-    collection.insert(json, function(err) {
-      if (err) console.log('Erreur');
-      console.log('Ajout' + json);
-      res.location('/collaborateurs');
+        // Récupération des données du formulaires "req.body.name" données du formuaire POST
+        var id = 6,
+            nom = req.body.nom,
+            prenom = req.body.prenom,
+            naissance = req.body.naissance,
+            entry = req.body.entry,
+            out = req.body.out,
+            poste = req.body.poste,
+            salaire = req.body.salaire,
+            numero = req.body.numero,
+            mail = req.body.mail;
+
+        // Insere dans un objet JSON
+        var json = {
+            'id': id,
+            'name': nom,
+            'prenom': prenom,
+            'naissance': naissance,
+            'poste': poste,
+            'salaire': salaire,
+            'entry': entry,
+            'out': out,
+            'numero': numero,
+            'mail': mail
+        };
+
+        // Insére le document
+        collection.insert(json, function (err) {
+            if (err) console.log('Erreur');
+            console.log('Ajout' + json);
+            db.close();
+        });
     });
-  });
+    // Redirige vers la page /collaborateurs
+    res.redirect('/collaborateurs');
 });
 
-app.get('/collaborateur/:nom/:prenom', function(req, res) {
-  res.render('employe.ejs');
+// Obtenir les informations d'un seul employé
+app.get('/collaborateur/:nom/:prenom', function (req, res) {
+    res.render('employe.ejs');
 });
 
-app.get('/ajouter', function(req, res) {
-  res.render('form.ejs');
-});
-
+// Lance l'application sur le port 8080
 app.listen(8080);
 console.log('Surprise MothaFucker ! (cf. Dexter)');
